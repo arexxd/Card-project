e95cb7b8a60bcedd67d0e020c083163f4ffb7da1
arexxd <60717897+arexxd@users.noreply.github.com>
2022-03-16 15:28:15 -0500

    Update card.java

--- card.java
+++ card.java
@@ -43,7 +43,7 @@ public class Card {
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-     /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        return suit;
       }
    
     /**
@@ -51,7 +51,7 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-     /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        return rank;
     }
    
       /**
@@ -59,7 +59,7 @@ public class Card {
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-     /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        return pointValue;
     }
    
     /** Compare this card with the argument.
@@ -69,7 +69,7 @@ public class Card {
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
-     /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        
     }
    
     /**
